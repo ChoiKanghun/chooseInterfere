5bf64ea36d0c09d0024e8901642aef4d59a93fe5
ChoiKanghun <rkdgns4562@naver.com>
2021-07-13 14:33:30 +0900

    init: 초기화. Firebase 콘솔 연결까지 완료.

---
+++ chooseInterfere/Pods/Target Support Files/Pods-chooseInterfere-chooseInterfereUITests/Pods-chooseInterfere-chooseInterfereUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chooseInterfere_chooseInterfereUITests : NSObject
+@end
+@implementation PodsDummy_Pods_chooseInterfere_chooseInterfereUITests
+@end
---
+++ chooseInterfere/chooseInterfere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chooseInterfere
+//
+//  Created by 최강훈 on 2021/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
